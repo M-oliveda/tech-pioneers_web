776e2f82805f37814d58e9f965bb90c0d9806eec
Mauricio Oliveda <maurioliveda@gmail.com>
2025-11-12 11:30:15 -0300

    :sparkles: Initialize carousel and modal modules in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -11,16 +11,42 @@
 // Import modules
 import Navigation from "./modules/navigation.js";
 import ThemeSwitcher from "./modules/theme-switcher.js";
+import Modal from "./modules/modal.js";
+import Carousel from "./modules/carousel.js";
 
 /**
  * Initialize application
  */
 function init() {
   // Initialize theme switcher
-  new ThemeSwitcher();
+  const themeSwitcher = new ThemeSwitcher();
+  themeSwitcher.init();
 
-  // Initialize navigation
+  // Initialize navigation (init is called automatically in constructor)
   new Navigation();
+
+  // Initialize modal
+  const modal = new Modal();
+  modal.init();
+
+  // Initialize carousels
+  const featuredCarousel = document.querySelector('[data-carousel="featured"]');
+  if (featuredCarousel) {
+    new Carousel(featuredCarousel, {
+      infinite: false,
+      autoplay: false,
+    });
+  }
+
+  const morePioneersCarousel = document.querySelector(
+    '[data-carousel="more-pioneers"]'
+  );
+  if (morePioneersCarousel) {
+    new Carousel(morePioneersCarousel, {
+      infinite: false,
+      autoplay: false,
+    });
+  }
 
   // Confirm initialization (development only)
   if (import.meta.env.DEV) {
