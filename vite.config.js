586efab79f830140858326256da3494ed91752ab
Mauricio Oliveda <maurioliveda@gmail.com>
2025-11-19 12:59:23 -0300

    :art: Use consistent string literals in Vite config

--- vite.config.js
+++ vite.config.js
@@ -18,11 +18,11 @@ export default defineConfig({
           const info = assetInfo.name.split(".");
           const ext = info[info.length - 1];
           if (/png|jpe?g|svg|gif|tiff|bmp|ico|webp/i.test(ext)) {
-            return `assets/images/${name}-[hash][extname]`;
+            return "assets/images/[name]-[hash][extname]";
           } else if (/woff|woff2|ttf|eot/i.test(ext)) {
-            return `assets/fonts/${name}-[hash][extname]`;
+            return "assets/fonts/[name]-[hash][extname]";
           }
-          return `assets/${name}-[hash][extname]`;
+          return "assets/[name]-[hash][extname]";
         },
         chunkFileNames: "assets/js/[name]-[hash].js",
         entryFileNames: "assets/js/[name]-[hash].js",
